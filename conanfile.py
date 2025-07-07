e01009f14377cc8118be70e566c0fcbaf7157b5f
Lucien <lucien.schreiber@terranum.ch>
2025-07-07 23:25:29 +0200

    Reverting to protobuf 3.21.12 to avoid abseil dependencies

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ class BasicwxWidgets(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     requires = ["wxwidgets/3.3.0@terranum-conan+wxwidgets/stable",
                 "gtest/1.15.0",
-                "protobuf/5.27.0",
+                "protobuf/3.21.12",
                 "zlib/1.3.1"]
 
     generators = "cmake_find_package", "cmake_paths"
@@ -14,7 +14,7 @@ class BasicwxWidgets(ConanFile):
             self.options["wxwidgets"].png = "system"
 
     def build_requirements(self):
-        self.tool_requires("protobuf/5.27.0")
+        self.tool_requires("protobuf/3.21.12")
 
     def imports(self):
         self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
