ab2fda2e6be1d3bda2d17ad3278a1e0e8a90654b
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2026-06-14 00:12:32 +0200

    Migrating project to conan 2.*

--- conanfile.py
+++ conanfile.py
@@ -1,24 +1,23 @@
-from conans import ConanFile, CMake
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
 
 class BasicwxWidgets(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = ["wxwidgets/3.3.0@terranum-conan+wxwidgets/stable",
-                "gtest/1.15.0",
-                "protobuf/3.21.12",
-                "zlib/1.3.1"]
+    requires = ["wxwidgets/3.3.2",
+                "gtest/1.17.0",
+                "protobuf/7.35.0",
+                "zlib/1.3.2"]
 
-    generators = "cmake_find_package", "cmake_paths"
+    generators = "CMakeDeps", "CMakeToolchain"
+    default_options = {
+        "wxwidgets/*:secretstore": False,
+    }
 
-    def configure(self):
-        if self.settings.os == "Linux":
-            self.options["wxwidgets"].png = "system"
+    def layout(self):
+        cmake_layout(self)
 
     def build_requirements(self):
-        self.tool_requires("protobuf/3.21.12")
-
-    def imports(self):
-        self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
-        self.copy("*.dylib*", dst="bin", src="lib")  # From lib to bin
+        self.tool_requires("protobuf/7.35.0")
 
     def build(self):
         cmake = CMake(self)
